cdc605a0c6dcc1b325d3afcfabf888973db01226
Nova <novarizqyundriansyah@gmail.com>
2025-03-02 20:27:32 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
                     target="_blank"
                     rel="noopener noreferrer">
                     <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-                    Examples
+                    Contoh
                 </a>
                 <a
                     className="flex items-center gap-2 hover:underline hover:underline-offset-4"
